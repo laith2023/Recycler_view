68849d56552d8967a731333709ba63a36098aec9
POST4 <post4.prog2@enetcom.usf.tn>
2023-11-09 16:46:09 +0100

    message

---
+++ app/src/main/java/com/example/enetcom/recyclerview/modele/Word.java
@@ -0,0 +1,16 @@
+package com.example.enetcom.recyclerview.modele;
+public class Word {
+    private String mWord;
+
+    public Word(String word){
+        this.mWord = word;
+    }
+
+    public void setWord(String word) {
+        this.mWord = word;
+    }
+
+    public String getWord() {
+        return mWord;
+    }
+}
